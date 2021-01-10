b0cc6f68c04fcfa62c587284b6584dc65ff85876
Takahiro Hasegawa <takio.sainokuni@gmail.com>
2021-01-10 22:15:18 +0900

    debug

--- link_prediction/model/hyperspherical_vae/distributions/__init__.py
+++ link_prediction/model/hyperspherical_vae/distributions/__init__.py
@@ -1,4 +1,4 @@
-from hyperspherical_vae.distributions.von_mises_fisher import VonMisesFisher
-from hyperspherical_vae.distributions.hyperspherical_uniform import (
+from link_prediction.model.hyperspherical_vae.distributions.von_mises_fisher import VonMisesFisher
+from link_prediction.model.hyperspherical_vae.distributions.hyperspherical_uniform import (
     HypersphericalUniform,
 )
