8c49cf3f3bfa4c811760cbe709ab8151f2aa5cf3
Takahiro Hasegawa <takio.sainokuni@gmail.com>
2021-01-21 14:31:21 +0900

    debug

--- analysis/optuna_optimization.py
+++ analysis/optuna_optimization.py
@@ -20,12 +20,14 @@ def get_optimizer(trial, model, args):
     if args.decode_model != 'Cat_Linear_Decoder':
         # weight_decay_bias = trial.suggest_loguniform('weight_decay_bias', args.weight_decay_bias_min, args.weight_decay_bias_max)
         # lr_bias = trial.suggest_uniform('lr_bias', args.lr_bias_min, args.lr_bias_max)
-        optimizer['decoder_bias'] = torch.optim.Adam(model.decode_model.bias.parameters(), weight_decay=weight_decay_bias, lr=lr_bias)
+        # optimizer['decoder_bias'] = torch.optim.Adam(model.decode_model.bias.parameters(), weight_decay=weight_decay_bias, lr=lr_bias)
+        optimizer['decoder_lins'] = torch.optim.Adam(model.decode_model.lins.parameters(), weight_decay=1e-2, lr=0.05)
+
 
     if args.decode_model == 'Cat_Linear_Decoder':
         weight_decay_decoder = trial.suggest_loguniform('weight_decay_decoder', args.weight_decay_decoder_min, args.weight_decay_decoder_max)
         lr_decoder = trial.suggest_uniform('lr_decoder', args.lr_decoder_min, args.lr_decoder_max)
-        optimizer['decoder_lins'] = torch.optim.Adam(model.decode_model.lins.parameters(), weight_decay=1e-2, lr=0.05)
+        optimizer['decoder_lins'] = torch.optim.Adam(model.decode_model.lins.parameters(), weight_decay=weight_decay_decoder, lr=lr_decoder)
     
     return optimizer
 
